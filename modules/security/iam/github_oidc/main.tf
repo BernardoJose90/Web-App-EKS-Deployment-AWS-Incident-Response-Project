1fd2d8fe9cc9ed8686e25097d7e18215884fd6b8
bernardo jose <bernardo.jose@bernardos-MacBook-Pro.local>
2025-10-15 07:23:23 +0100

    test3

--- modules/security/iam/github_oidc/main.tf
+++ modules/security/iam/github_oidc/main.tf
@@ -1,11 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.16"
+    }
+  }
+}
+
 data "aws_caller_identity" "current" {}
 
-variable "github_org" {}
-variable "github_repo" {}
-variable "github_branch" {
-  default = "main"
-}
-variable "kms_key_id" {}
+
 
 resource "aws_iam_role" "github_actions_role" {
   name = "GitHubActionsRole"
@@ -28,12 +32,72 @@ resource "aws_iam_role" "github_actions_role" {
     ]
   })
 
-
-  tags = {
-    Environment = "Production"
-    ManagedBy   = "Terraform"
-  }
 }
+
+resource "aws_iam_policy" "github_actions_policy" {
+  name        = "GitHubActionsPolicy"
+  description = "Least privilege for Terraform + EKS CI workflow"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeRouteTables",
+          "ec2:CreateRouteTable",
+          "ec2:DeleteRouteTable",
+          "ec2:CreateSubnet",
+          "ec2:DeleteSubnet",
+          "ec2:CreateTags",
+          "ec2:DeleteTags",
+          "ec2:AssociateRouteTable",
+          "ec2:DisassociateRouteTable",
+          "ec2:AttachInternetGateway",
+          "ec2:CreateInternetGateway"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:CreateCluster",
+          "eks:DescribeCluster",
+          "eks:DeleteCluster",
+          "eks:ListClusters"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ]
+        Resource = "arn:aws:s3:::my-ci-cd-artifacts/prod/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:Encrypt",
+          "kms:GenerateDataKey"
+        ]
+        Resource = "arn:aws:kms:eu-west-2:${data.aws_caller_identity.current.account_id}:key/${var.kms_key_id}"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Resource = "arn:aws:secretsmanager:eu-west-2:${data.aws_caller_identity.current.account_id}:secret:prod/kms-key-*"
+      }
+    ]
+  })
+}
+
 
 resource "aws_iam_role_policy_attachment" "attach_github_policy" {
   role       = aws_iam_role.github_actions_role.name
